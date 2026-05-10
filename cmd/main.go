11bac145ab319a2e484c7197f51f2c65316cdddf
Eric <real.dassy@gmail.com>
2026-05-10 21:54:01 +0900

    [Feat] Client Room 라우팅 로직 추가
    기존 에코 관련 로직 제거

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"temp-ws/internal/client"
+	"temp-ws/internal/message"
+	"temp-ws/internal/room"
 
 	"github.com/coder/websocket"
+	"github.com/google/uuid"
 )
 
 func main() {
+	hub := room.NewHub()
+
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := websocket.Accept(w, r, nil)
 		if err != nil { return }
 
 		defer func() { _ = conn.Close(websocket.StatusNormalClosure, "") }()
 
-		cli := client.Client { Conn: conn }
+		roomID := r.URL.Query().Get("room")
+		var currentRoom *room.Room
+
+		if roomID == "" {
+			currentRoom, roomID = hub.CreateRoom()
+		} else {
+			currentRoom = hub.FindRoom(roomID)
+			if currentRoom == nil {
+				conn.Close(websocket.StatusPolicyViolation, "room not found")
+				return
+			}
+		}
+
+		cli := client.Client { ID: uuid.NewString(), Conn: conn, Send: make(chan *message.Message, 1) }
+		currentRoom.Register(&cli)
 		cli.Run()
 	})
 
