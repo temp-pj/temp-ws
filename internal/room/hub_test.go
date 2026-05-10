1db2e9c8ffcf887965fba13e794c49df747adcc7
Eric <real.dassy@gmail.com>
2026-05-10 18:57:50 +0900

    [Test] 동시성 테스트 추가

--- internal/room/hub_test.go
+++ internal/room/hub_test.go
@@ -1,6 +1,9 @@
 package room
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCreateRoom(t *testing.T) {
 	hub := NewHub()
@@ -36,4 +39,24 @@ func TestFindRoom_NotFound(t *testing.T) {
 	if room != nil {
 		t.Error("있을리 없는 방이 찾아짐 FindRoom 로직 자체에 문제 있음")
 	}
+}
+
+func TestConcurrentAccess(t *testing.T) {
+	hub := NewHub()
+    var wg sync.WaitGroup
+
+    for i := 0; i < 10; i++ {
+        wg.Add(1)
+        go func() {
+            defer wg.Done()
+            roomID := hub.CreateRoom()
+            room := hub.FindRoom(roomID)
+            if room == nil {
+                t.Error("동시 생성된 방을 찾지 못함")
+            }
+            hub.DeleteRoom(roomID)
+        }()
+    }
+
+    wg.Wait()
 }
