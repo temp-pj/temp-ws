70212ea1b4d043e1c22e43e2e912fb785eccac24
Eric <real.dassy@gmail.com>
2026-05-10 21:54:21 +0900

    [Feat] Client 송수신 로직 추가
    기존 에코 로직 제거

--- internal/client/client.go
+++ internal/client/client.go
@@ -2,21 +2,30 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"temp-ws/internal/message"
 
 	"github.com/coder/websocket"
 )
 
 type Client struct {
+	ID string
 	Conn *websocket.Conn
 	Send chan *message.Message
 }
 
 func (c *Client) Run() { 
 	ctx := context.Background()
+
+	go func() {
+		for msg := range c.Send {
+			data, _ := json.Marshal(msg)
+			c.Conn.Write(ctx, websocket.MessageText, data)
+		}
+	}()
+
 	for {
-		msgType, data, err := c.Conn.Read(ctx)
-		if err != nil { return }
-		_ = c.Conn.Write(ctx, msgType, data)
+		_, _, err := c.Conn.Read(ctx)
+        if err != nil { return }
 	}
 }
