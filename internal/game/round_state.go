a4df61ee98c3b2cf585f51b583655922d6297890
Eric <real.dassy@gmail.com>
2026-05-20 22:30:35 +0900

    [Refactor] 더 이상 사용되지 않는 RoundState 케이스 제거(Loading, Finished)

--- internal/game/round_state.go
+++ internal/game/round_state.go
@@ -4,24 +4,18 @@ type RoundState int
 
 const (
 	Idle RoundState = iota
-	Loading
 	Playing
 	Result
-	Finished
 )
 
 func (s RoundState) String() string {
 	switch s {
 		case Idle:
 			return "idle"
-		case Loading:
-			return "loading"
 		case Playing:
 			return "playing"
 		case Result:
 			return "result"
-		case Finished:
-			return "finished"
 	}
 
 	return "unknown"
