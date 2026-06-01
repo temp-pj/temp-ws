029df678b5338aad16f9c39659b8d9228c4db251
Eric <real.dassy@gmail.com>
2026-06-01 12:51:35 +0900

    [Feat] Song 구조체 업데이트(MusicBrainz 캐시 DB 기반)

--- internal/music/song.go
+++ internal/music/song.go
@@ -1,14 +1,12 @@
 package music
 
+import "time"
+
 
 type Song struct {
-	Title string		`json:"title"`
-	Artist string		`json:"artist"`
-	PreviewURL string	`json:"previewURL"`
-	WrappedID string	`json:"wrappedID"`
-}
-
-func (s Song) WrappedURL()string {
-	/// TO-DO: 프록시 구현 예정 "https://audio.apple.com/preview/park-hyoshin-wildflower.m4a" -> "https://서버.com/audio/a3f8x9k2"
-	return s.PreviewURL
+	MBID string				`json:"mbid"`
+	Title string			`json:"title"`
+	ISRC string				`json:"isrc"`
+	Artist string			`json:"artist"`
+	ReleaseDate time.Time	`json:"release_date"`
 }
