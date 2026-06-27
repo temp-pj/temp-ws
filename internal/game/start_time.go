e5395138def1a29e49fd8184199fc3bb785bfefb
Eric <real.dassy@gmail.com>
2026-06-27 17:52:36 +0900

    [Fix] 시작 시간이 항상 0초부터 되는 현상 수정(ms -> sec)

--- internal/game/start_time.go
+++ internal/game/start_time.go
@@ -2,8 +2,7 @@ package game
 
 import "math/rand/v2"
 
-func generateStartTime(durationMs int, timeLimitSec int)int {
-	durationSec := durationMs / 1000
+func generateStartTime(durationSec int, timeLimitSec int) int {
 	maxSec := durationSec - 10 - timeLimitSec
 
 	if maxSec <= 0 { return 0 }
